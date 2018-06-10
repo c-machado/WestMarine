083be6179082670fd37cf639e3524bace96639a0
Carolina Machado <machadoca@google.com>
2018-06-10 16:39:42 -0500

    update register feature file and clean up the login form

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -16,7 +16,7 @@ public class BaseClass {
     public WebDriver getBrowser() {
         if(browser == null) {
             browser = new ChromeDriver();
-            browser.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         }
         return browser;
     }
